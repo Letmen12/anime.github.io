363d8bfb588883b234f050564bf315c1022e2ded
cipher <fynhacker@gmail.com>
2023-12-12 02:40:50 +0800

    dahiad

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
         searchForm.classList.toggle("show");
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const animeItems = document.querySelectorAll('.anime-item');
+
+    animeItems.forEach(function (item) {
+        item.addEventListener('click', function () {
+            this.classList.toggle('enlarged');
+        });
+    });
+});
